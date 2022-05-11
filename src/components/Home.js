5aa1d4749084e7aa26677dca9ac05d303dc855fb
ReitoSerizawa <s.reitiger@gmail.com>
2022-05-11 14:37:47 -0400

    logo size changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,24 +28,11 @@ function Home({ clothes }) {
 
     return (
         <div>
-<<<<<<< HEAD
-        <div className='main' style={{ 
-            backgroundImage: `url("https://c0.wallpaperflare.com/preview/827/1022/479/japan-shibuya-night-life.jpg")`,
-            borderRadius: '10px',
-          }}>
-            <h1 style={BANNER_STYLE}>Not Uniqlo </h1>  
-            <div className="home-container">
-                </div>
-            </div>
-            <h2 style={{ margin: '50px' }}>Latest Additions</h2>
-            <div className="grid">
-=======
             <ImageOne /> 
             <ImageOneDesc />
             <ImageTwo />
             <h3>check out these recent additions</h3>
             <div class="grid">
->>>>>>> a9bbcff756e5289b74f07ba34f8ff8283c71739f
                      {newAdditions.map((card) => (
                     <FlipCard key={card.id} card={card} />))}
                     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
