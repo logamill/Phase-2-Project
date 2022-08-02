fc190ea8d872dcad91e6133fc5cdc93be43071fe
Logan Miller <loganmiller214@gmail.com>
2022-08-02 16:00:47 -0400

    updated icon and website desc as well as fixed some images and item add form

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -30,17 +30,12 @@ function ItemForm({ setUpdatePage }) {
         const redirectURL = formData.category.toLowerCase().includes('accessor') ? `accessories` : `${formData.category}`
 
         history.push(`/${redirectURL}`)
-
-        setFormData({category: "Tops", name: "", image: "", brand: "", color: "#ffffff", price: "", size: "", favorite: false})
     }
-
     const backgroundStyle = {backgroundImage: 'url("https://diana.divi-den.com/wp-content/uploads/2019/01/diana-noble-header-image2.jpg")',
     height:"400px", width:"100%", padding: "20px"}
-
     return (
         <>
                 <ItemFormImg />
-        
     <div className='main'>
         {/* <div
             className="p-5 text-center bg-image"
@@ -56,72 +51,40 @@ function ItemForm({ setUpdatePage }) {
                 </div>
             {/* </div> */}
         </div>
-
-
         <form className="form" id='form' onSubmit={handleSubmit}>
-
         <div className="form-group row" style={{padding:"15px"}}>
-
             <label className="col-sm-1 col-form-label" for="name">Type</label>
-
             <div className="col-sm-11">
-
-            <input className="form-control" type="text" name="name" placeholder="Hoodie"
-            value={formData.name}
-            onChange={handleChange}
-            />
+                <input className="form-control" type="text" name="name" placeholder="Hoodie"
+                value={formData.name}
+                onChange={handleChange} />
             </div>
-        
         </div>
-
         <div className="form-group row" style={{padding:"15px"}}>
-            
             <label className="col-sm-1 col-form-label" for="image">Image</label>
-
             <div className="col-sm-11">
-
-            <input className="form-control" type="text" name="image" placeholder="https://www.sample.jpg"
-            value={formData.image}
-            onChange={handleChange}            
-            />
-
+                <input className="form-control" type="text" name="image" placeholder="https://www.sample.jpg"
+                value={formData.image}
+                onChange={handleChange} />
             </div>
-
         </div>
-
         <div className="form-group row" style={{padding:"15px"}}>
-
             <label className="col-sm-1 col-form-label" for="brand">Brand</label>
-
             <div className="col-sm-7">
-
-            <input className="form-control" type="text" name="brand" placeholder="Uniqlo"
-            value={formData.brand}
-            onChange={handleChange}                      
-            />
-
+                <input className="form-control" type="text" name="brand" placeholder="Uniqlo"
+                value={formData.brand}
+                onChange={handleChange} />
             </div>
-
-            <label className="col-sm-1 col-form-label" for="price">Price</label>
-
+                <label className="col-sm-1 col-form-label" for="price">Price</label>
             <div className="col-sm-3">
-
-            <input className="form-control" type="number" name="price" placeholder="0.00" step="0.01" min='0.00'
-            value={formData.price}
-            onChange={handleChange}                
-            />
-
+                <input className="form-control" type="number" name="price" placeholder="0.00" step="0.01" min='0.00'
+                value={formData.price}
+                onChange={handleChange} />
             </div>
-
         </div>
-
-
         <div className="form-group row" style={{padding:"15px"}}>
-
             <label className="col-sm-1 col-form-label" for="category">Category</label>
-
             <div className="col-sm-3">
-            
                 <select className="form-control"
                     name="category" id="category"
                     value={formData.category}
@@ -132,39 +95,24 @@ function ItemForm({ setUpdatePage }) {
                         <option value="Outerwear">Outerwear</option>
                         <option value="Accessories & Shoes">Accessories & Shoes</option>
                 </select>
-
             </div>
-
             <label className="col-sm-1 col-form-label" for="size">Size</label>
-
             <div className="col-sm-3">
-
                 <input className="form-control" type="text" name="size" placeholder="M"
                 value={formData.size}
                 onChange={handleChange}                  
                 />
-
             </div>
-
-
             <label className="col-sm-1 col-form-label" for="Color">Color</label>
-
             <div className="col-sm-3">
-
                 <input className="form-control-color" type="color" name="color" placeholder="Color"
                 value={formData.color}
                 onChange={handleChange}    
                 />
-
             </div>
-
         </div>
-
             <button type="submit" className="btn btn-outline-dark">Add</button>
-            
         </form>
-
-
     </div>
     </>
     )
